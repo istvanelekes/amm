cdc2ec900ca73db1f8bd1d5b07718989ef707cb1
Istvan Elekes <istvan.elekes@partner.rolls-roycemotorcars.com>
2024-10-15 18:21:38 +0300

    Load data into redux

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,7 @@ import { ethers } from 'ethers'
 import Navigation from './Navigation';
 import Loading from './Loading';
 
-import { loadProvider, loadNetwork, loadAccount } from '../store/interactions'
-
-// ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
-
-// Config: Import your network config here
-// import config from '../config.json';
+import { loadProvider, loadNetwork, loadAccount, loadTokens, loadAMM } from '../store/interactions'
 
 function App() {
   const dispatch = useDispatch()
@@ -26,6 +20,10 @@ function App() {
 
     // Fetch accounts
     await loadAccount(dispatch)
+
+    // Initiate contracts
+    await loadTokens(provider, chainId, dispatch)
+    await loadAMM(provider, chainId, dispatch)
   }
 
   useEffect(() => {
